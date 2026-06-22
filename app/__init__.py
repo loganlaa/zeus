054df3a1e847ce6307632d028e58b28fe995477c
Logan <logan.alvima@gmail.com>
2026-06-21 23:24:49 -0300

    Add SQLAlchemy and Project model

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,15 @@ def create_app():
 
     from app.routes.web import web
     from app.routes.api import api
+    from app.extensions import db
+    from app.models.project import Project
 
     app.register_blueprint(web)
     app.register_blueprint(api)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "sqlite:///zeus.db"
+
+    db.init_app(app)
 
     return app
